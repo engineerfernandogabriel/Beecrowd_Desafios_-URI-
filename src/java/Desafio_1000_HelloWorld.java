9c1e51b9b2726c29a45a073f8fa80817f70a6eb6
Nando Gabriel <fernandoramos.sjc@gmail.com>
2022-03-25 00:40:25 -0300

    URI 1001

---
+++ src/java/Desafio_1000_HelloWorld.java
@@ -0,0 +1,21 @@
+package java;
+
+/*
+ O seu primeiro programa em qualquer linguagem de programa��o normalmente � o "Hello World!". 
+ Neste primeiro problema tudo o que voc� precisa fazer � imprimir esta mensagem na tela.
+
+ENTRADA
+ Este problema n�o possui nenhuma entrada.
+
+SAíDA
+ Voc� deve imprimir a mensagem "Hello World!" e em seguida o final de linha, conforme o exemplo abaixo.
+*/
+
+public class Desafio_1000_HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/java/Desafio_1001_ExtremamenteBasico.java
@@ -0,0 +1,10 @@
+package java;
+
+public class Desafio_1001_ExtremamenteBasico {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
