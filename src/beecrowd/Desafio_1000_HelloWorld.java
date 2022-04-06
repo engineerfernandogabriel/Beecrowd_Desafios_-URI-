7cc4e6f2dbd85d28fb54c43ee462c2c83625c1bd
Nando Gabriel <fernandoramos.sjc@gmail.com>
2022-04-05 23:00:42 -0300

    URI-1002

---
+++ src/beecrowd/Desafio_1000_HelloWorld.java
@@ -0,0 +1,21 @@
+package src.beecrowd;
+
+/*
+ O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!".
+ Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+ENTRADA
+ Este problema não possui nenhuma entrada.
+
+SAíDA
+ Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha, conforme o exemplo abaixo.
+*/
+
+public class Desafio_1000_HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/beecrowd/Desafio_1001_ExtremamenteBasico.java
@@ -0,0 +1,36 @@
+package src.beecrowd;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/*
+ Leia 2 valores inteiros e armazene-os nas vari�veis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X.
+ Não apresente mensagem alguma além daquilo que está sendo especificado e não esqueçaa de imprimir o fim de linha após o resultado,
+ caso contrário, você receberá "Presentation Error".
+
+ENTRADA
+ A entrada contém 2 valores inteiros.
+
+SAÍDA
+ Imprima a mensagem "X = " (letra X maiúscula) seguido pelo valor da variável X e pelo final de linha.
+ Cuide para que tenha um espaço antes e depois do sinal de igualdade, conforme o exemplo abaixo.
+*/
+
+public class Desafio_1001_ExtremamenteBasico {
+
+	public static void main(String[] args) throws IOException {
+		int soma, A, B;
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		A = leitor.nextInt();
+		B = leitor.nextInt();
+				
+		soma = A + B;
+		
+		leitor.close();
+		
+		System.out.println("X = " + soma);
+	}
+
+}
