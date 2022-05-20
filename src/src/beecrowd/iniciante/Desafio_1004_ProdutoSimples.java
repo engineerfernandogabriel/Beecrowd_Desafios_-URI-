9747a7ca59092de0240878283eb8af057b5050ec
Nando Gabriel <fernandoramos.sjc@gmail.com>
2022-05-19 22:36:21 -0300

    Day 02 - Desafio Java | SpringBoot

---
+++ src/src/beecrowd/iniciante/Desafio_1004_ProdutoSimples.java
@@ -0,0 +1,37 @@
+package src.src.beecrowd.iniciante;
+
+/*
+ Leia dois valores inteiros.
+ A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD.
+ A seguir mostre a variável PROD com mensagem correspondente.
+
+ENTRADA
+ O arquivo de entrada contém 2 valores inteiros.
+
+SAÍDA
+ Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade.
+ Não esqueça de imprimir o fim de linha após o produto, caso contrário seu programa apresentará a mensagem: “Presentation Error”.
+ */
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Desafio_1004_ProdutoSimples {
+
+    public static void main(String[] args) throws IOException {
+        int A, B, PROD;
+
+        Scanner leitor = new Scanner(System.in);
+
+        A = leitor.nextInt();
+        B = leitor.nextInt();
+
+        PROD = A * B;
+
+        leitor.close();
+
+        System.out.println("PROD = " + PROD);
+
+
+    }
+}
