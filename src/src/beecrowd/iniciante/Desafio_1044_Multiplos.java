98055a43acc3a886856c9be636d41e3ffe1a2551
Nando Gabriel <fernandoramos.sjc@gmail.com>
2022-06-03 22:55:56 -0300

    Day 17 - Desafio Java | SpringBoot

---
+++ src/src/beecrowd/iniciante/Desafio_1044_Multiplos.java
@@ -0,0 +1,43 @@
+package src.src.beecrowd.iniciante;
+
+/*
+ Leia 2 valores inteiros (A e B).
+ Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", indicando se os valores lidos são múltiplos entre si.
+
+ENTRADA
+ A entrada contém valores inteiros.
+
+SAÍDA
+ A saída deve conter uma das mensagens conforme descrito acima.
+ */
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Desafio_1044_Multiplos {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner teclado = new Scanner(System.in);
+
+        int a = teclado.nextInt();
+        int b = teclado.nextInt();
+
+        teclado.close();
+
+        if (a > b) {
+            if ((a % b) == 0) {
+                System.out.println("Sao Multiplos");
+            } else {
+                System.out.println("Nao sao Multipplos");
+            }
+        }
+
+        if ((b % a) == 0) {
+            System.out.println("Sao Multiplos");
+        } else {
+            System.out.println("Nao sao Multiplos");
+        }
+
+    }
+}
