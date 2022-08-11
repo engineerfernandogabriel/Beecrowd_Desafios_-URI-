d9858321bca8046ea474ed9d7e4d14a47829f5bf
Nando Gabriel <fernandoramos.sjc@gmail.com>
2022-08-10 22:45:10 -0300

    Day 87 - Desafio Java | SpringBoot

---
+++ src/src/beecrowd/iniciante/Desafio_1146_SequenciasCrescentes.java
@@ -0,0 +1,42 @@
+package src.src.beecrowd.iniciante;
+
+/*
+ Este programa deve ler uma variável inteira X inúmeras vezes (deve parar quando o valor no arquivo de entrada for igual a zero).
+
+ Para cada valor lido imprima a sequência de 1 até X, com um espaço entre cada número e seu sucessor.
+
+ Obs: cuide para não deixar espaço em branco após o último valor apresentado na linha ou você receberá Presentation Error.
+
+ENTRADA
+ O arquivo de entrada contém vários números inteiros. O último número no arquivo de entrada é 0.
+
+SAÍDA
+ Para cada número N do arquivo de entrada deve ser impressa uma linha de 1 até N, conforme o exemplo abaixo.
+
+ Não deve haver espaço em branco após o último valor da linha.
+ */
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Desafio_1146_SequenciasCrescentes {
+
+    public static void main(String[] args) throws IOException{
+
+        Scanner input = new Scanner(System.in);
+        boolean test = true;
+
+        while(test){
+            int numberSequencie = input.nextInt();
+
+            if(numberSequencie > 0){
+              for(int i = 1; i <= numberSequencie; i++){
+                  if(i != numberSequencie) System.out.printf("%d ", i);
+                  else System.out.printf("%d\n", i);
+              }
+            } else{
+                test = false;
+            }
+        }
+    }
+}
