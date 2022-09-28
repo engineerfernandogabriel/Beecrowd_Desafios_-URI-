20d28dc61a865b5b99301d1be47dd2d229c7d5f9
Nando Gabriel <fernandoramos.sjc@gmail.com>
2022-09-28 00:58:02 -0300

    BeeCrowd 1181

---
+++ src/src/beecrowd/iniciante/Desafio_1181_LinhaNaMatriz.java
@@ -0,0 +1,51 @@
+package src.src.beecrowd.iniciante;
+
+/*
+ Neste problema você deve ler um número, indicando uma linha da matriz na qual uma operação deve ser realizada, um caractere maiúsculo, indicando a operação que será realizada,
+e todos os elementos de uma matriz M[12][12].
+
+ Em seguida, calcule e mostre a soma ou a média dos elementos que estão na área verde da matriz, conforme for o caso.
+
+ A imagem abaixo ilustra o caso da entrada do valor 2 para a linha da matriz, demonstrando os elementos que deverão ser considerados na operação.
+
+ENTRADA
+ A primeira linha de entrada contem um número L (0 ≤ L ≤ 11) indicando a linha que será considerada para operação.
+
+ A segunda linha de entrada contém um único caractere Maiúsculo T ('S' ou 'M'), indicando a operação (Soma ou Média) que deverá ser realizada com os elementos da matriz.
+
+ Seguem os 144 valores de ponto flutuante que compõem a matriz, sendo que a mesma é preenchida linha por linha, da linha 0 até a linha 11, sempre da esquerda para a direita.
+
+SAÍDA
+ Imprima o resultado solicitado (a soma ou média), com 1 casa após o ponto decimal.
+ */
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Desafio_1181_LinhaNaMatriz {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner input = new Scanner(System.in);
+
+        double[][] matriz = new double[3][3];
+
+        int line = input.nextInt();
+        String operation = input.next();
+        double sum = 0;
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                double values = input.nextDouble();
+                matriz[i][j] = values;
+            }
+        }
+
+        for (int x = 0; x < matriz[line].length; x++) {
+            sum += matriz[line][x];
+        }
+
+        if (operation.equalsIgnoreCase("S")) System.out.printf("%.1f", sum);
+        else System.out.printf("%.1f", (sum / 3));
+    }
+}
